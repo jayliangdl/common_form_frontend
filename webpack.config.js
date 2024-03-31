06b0ad754c29cb6cccc3ab4c30cba31b266782c9
CN087005 <jay.liang@amway.com>
2024-03-31 23:32:50 +0800

    xx

--- webpack.config.js
+++ webpack.config.js
@@ -4,44 +4,53 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new Dotenv(),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html' // 指向你的模板文件
-    }),
-    new webpack.DefinePlugin({
-    })
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
+module.exports = (env,argv)=>{
+  // 判断当前是否为生产模式
+  const isProduction = argv.mode === 'production';
+  // 根据是生产模式还是开发模式，选择不同的.env文件
+  const envPath = isProduction ? './.env.production' : './.env';
+
+  return {
+    entry: './src/main.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
     },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'vue-style-loader',
+            'css-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new Dotenv({
+        path: envPath, // 加载指定的.env文件
+      }),
+      new HtmlWebpackPlugin({
+        template: 'src/index.html' // 指向你的模板文件
+      }),
+      new webpack.DefinePlugin({
+      })
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+    }
   }
 };
